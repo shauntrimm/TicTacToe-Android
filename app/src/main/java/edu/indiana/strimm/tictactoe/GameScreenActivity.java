a56914c810ab065a66fa63c3d7f9f237f5863fed
shauntrimm <shauntrimm98@gmail.com>
2020-02-29 00:54:30 -0500

    Added menu and toolbar to game game_screen

--- app/src/main/java/edu/indiana/strimm/tictactoe/GameScreenActivity.java
+++ app/src/main/java/edu/indiana/strimm/tictactoe/GameScreenActivity.java
@@ -1,7 +1,18 @@
 package edu.indiana.strimm.tictactoe;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /*
 GameScreenActivity.java
@@ -13,11 +24,39 @@ Last Modified On: 2-28-2020
 Assignment: A290 Final Project Phase 3
  */
 
-public class GameScreenActivity extends Activity {
+public class GameScreenActivity extends AppCompatActivity {
 
+    @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.game_screen);
+        setContentView(R.layout.game_screen_toolbar);
+        Toolbar toolbar = findViewById(R.id.game_toolbar);
+        setSupportActionBar(toolbar);
 
+        //keeps the title text from showing
+        getSupportActionBar().setTitle(null);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_game_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //If the About icon is clicked
+        if (id == R.id.action_about_game) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
