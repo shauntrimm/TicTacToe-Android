65e2a6ae8b02b53570c6b29c461aab3b334078d6
shauntrimm <shauntrimm98@gmail.com>
2020-02-29 22:36:00 -0500

    Added place holders for the game grid

--- app/src/main/java/edu/indiana/strimm/tictactoe/MainActivity.java
+++ app/src/main/java/edu/indiana/strimm/tictactoe/MainActivity.java
@@ -2,14 +2,8 @@ package edu.indiana.strimm.tictactoe;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
